5e649984d7b0ec24b258f8378b4d9be7dd91b8b5
𝐌 𝐈 𝐙 𝐔 𝐌 𝐀 𝐑 𝐔 <mizumaruofficialdump@gmail.com>
2025-12-06 21:38:33 +0800

    Update image URL for Arceus X configuration

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@ window.scriptData = [
     description: "Arceus X by SPDM Team: powerful Roblox executor with advanced features, fast updates, and a clean UI.",
     category: "FEATURED",
     tags: ["arceusx", "spdmteam", "roblox", "ai-powered"],
-    image: "https://tr.rbxcdn.com/180DAY-91f8ec73480a974786806c66d92dd30e/768/432/Image/Webp/noFilter",
+    image: "https://files.catbox.moe/55tkbq.png",
     discord: "https://discord.gg/lugia",
     download: "https://arceusx.com/download",
     platforms: ["Android", "PC"],
